7c1b07f546e9f1ef2e4dd17c5a812293f479f5c9
wangrui <laofyx@163.com>
2024-03-23 13:36:13 +0800

    [fix] esbuild

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,14 +1,12 @@
 import { build } from 'esbuild'
 
 await build({
-  entryPoints: ["./src/extension.ts"],
-	bundle: true,
-	format: "cjs",
-	minify: true,
-	target: "node16.18",
-	outfile: "./dist/src/index.js",
-	external: [
-    "vscode",
-		"node:path",
-	],
+  entryPoints: ['./src/extension.ts'],
+  bundle: true,
+  format: 'cjs',
+  minify: true,
+  target: 'node16.18',
+  outfile: './dist/extension.js',
+  platform: 'node',
+  external: ['vscode']
 })
