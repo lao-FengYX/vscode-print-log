26f1b9355036b44889960337ebf0d6fb75c4bd4a
wangrui <laofyx@163.com>
2024-03-25 18:33:51 +0800

    [fix] 匹配`console`调用换行的情况

--- src/utils/remove.ts
+++ src/utils/remove.ts
@@ -1,11 +1,18 @@
-import * as vscode from 'vscode'
+import {
+  commands,
+  workspace,
+  window,
+  Range,
+  Position,
+  TextDocument
+} from 'vscode'
 import { Extension, getConfig, getTextEditor } from '.'
 import Logger from './logger'
 
 let format = getConfig('clear.format')
 let removeEmptyLine = getConfig('clear.deleteCommentsAndEmptyLine')
 
-vscode.workspace.onDidChangeConfiguration((e) => {
+workspace.onDidChangeConfiguration((e) => {
   if (e.affectsConfiguration(Extension.name)) {
     format = getConfig('clear.format')
     removeEmptyLine = getConfig('clear.deleteCommentsAndEmptyLine')
@@ -13,49 +20,102 @@ vscode.workspace.onDidChangeConfiguration((e) => {
 })
 
 /**
+ * 返回console的范围
+ */
+const findConsoleRange = (document: TextDocument, text: string) => {
+  const consoleReg =
+    /((window|global|globalThis)\.)?console\.(log|debug|info|warn|error|assert|clear|dir|dirxml|trace|table|group|groupCollapsed|groupEnd|time|timeEnd|timeLog|timeStamp|profile|profileEnd|count|countReset)/g
+
+  // 正则内的括号不进行记录
+  const reg = /\/.+\//g
+  text = text.replace(reg, (s) => ''.padEnd(s.length, ' '))
+
+  let result: Range[] = []
+  let match
+
+  while ((match = consoleReg.exec(text))) {
+    const sourceLength = match[0].length
+    const start = sourceLength + match.index
+    let end = start
+    let count = 0
+
+    if (text.slice(start, start + 1) !== '(') {
+      continue
+    }
+
+    while (end < text.length) {
+      let str = text[end]
+      if (str === '(') {
+        count++
+      } else if (str === ')') {
+        count--
+      }
+
+      if (count === 0) {
+        break
+      }
+      end++
+    }
+
+    if (text[end + 1] === ';') {
+      end++
+    }
+
+    const newRange = new Range(
+      document.positionAt(match.index),
+      document.positionAt(end + 1)
+    )
+    // 检查范围是否重叠
+    if (!result.some((range) => range.intersection(newRange))) {
+      result.push(newRange)
+    }
+  }
+
+  return result
+}
+
+/**
  * 删除console
  */
 export const removeAllConsole = () =>
-  vscode.commands.registerCommand('print.cleanConsole', () => {
+  commands.registerCommand('print.cleanConsole', () => {
     const editor = getTextEditor()
     if (!editor) {
       return
     }
     const document = editor.document
-    const resourceLength = document.lineCount
 
-    const consoleReg =
-      /((window|global|globalThis)\.)?console\.(log|debug|info|warn|error|assert|clear|dir|dirxml|trace|table|group|groupCollapsed|groupEnd|time|timeEnd|timeLog|timeStamp|profile|profileEnd|count|countReset)\((.*)\);?/g
     let text = document.getText()
-    let count = 0
 
     try {
       const timeStamp = performance.now()
-      text = text.replace(consoleReg, () => {
-        count++
-        return ''
-      })
+      const rangeList = findConsoleRange(document, text)
+
       editor
         .edit((builder) => {
-          const documentStart = new vscode.Position(0, 0)
-          const documentEnd = document.lineAt(resourceLength - 1).range.end
-          builder.replace(new vscode.Range(documentStart, documentEnd), text)
+          rangeList.forEach((range) => {
+            builder.replace(range, '')
+          })
         })
         .then((success) => {
           if (success) {
             Logger.info(
-              `remove console time ${Math.ceil(performance.now() - timeStamp)}ms`
+              `remove console time ${Math.ceil(
+                performance.now() - timeStamp
+              )}ms`
             )
 
-            vscode.window.showInformationMessage(`remove ${count} console ✅`)
+            window.showInformationMessage(
+              `remove ${rangeList.length} console ✅`
+            )
 
             format // 触发vscode的格式化
-              ? vscode.commands.executeCommand('editor.action.formatDocument')
+              ? commands.executeCommand('editor.action.formatDocument')
               : null
           }
         })
     } catch (error) {
-      vscode.window.showErrorMessage(`remove console error ❌`)
+      window.showErrorMessage(`remove console error ❌`)
     }
   })
 
@@ -63,7 +123,7 @@ export const removeAllConsole = () =>
  * 删除注释
  */
 export const removeAllComment = () =>
-  vscode.commands.registerCommand('print.removeAllComment', () => {
+  commands.registerCommand('print.removeAllComment', () => {
     const editor = getTextEditor()
     if (!editor) {
       return
@@ -83,28 +143,30 @@ export const removeAllComment = () =>
 
       editor
         .edit((builder) => {
-          const documentStart = new vscode.Position(0, 0)
+          const documentStart = new Position(0, 0)
           const documentEnd = document.lineAt(document.lineCount - 1).range.end
-          builder.replace(new vscode.Range(documentStart, documentEnd), text)
+          builder.replace(new Range(documentStart, documentEnd), text)
         })
         .then((success) => {
           if (success) {
             Logger.info(
-              `remove comments time ${Math.ceil(performance.now() - timeStamp)}ms`
+              `remove comments time ${Math.ceil(
+                performance.now() - timeStamp
+              )}ms`
             )
 
-            vscode.window.showInformationMessage(`remove ${count} comments ✅`)
+            window.showInformationMessage(`remove ${count} comments ✅`)
 
             format
-              ? vscode.commands.executeCommand('editor.action.formatDocument')
+              ? commands.executeCommand('editor.action.formatDocument')
               : null
             removeEmptyLine
-              ? vscode.commands.executeCommand('print.removeAllEmptyLine')
+              ? commands.executeCommand('print.removeAllEmptyLine')
               : null
           }
         })
     } catch (error) {
-      vscode.window.showErrorMessage(`remove comment error ❌`)
+      window.showErrorMessage(`remove comment error ❌`)
     }
   })
 
@@ -112,7 +174,7 @@ export const removeAllComment = () =>
  * 删除空行
  */
 export const removeAllEmptyLine = () =>
-  vscode.commands.registerCommand('print.removeAllEmptyLine', () => {
+  commands.registerCommand('print.removeAllEmptyLine', () => {
     const editor = getTextEditor()
     if (!editor) {
       return
@@ -128,9 +190,9 @@ export const removeAllEmptyLine = () =>
 
       editor
         .edit((builder) => {
-          const documentStart = new vscode.Position(0, 0)
+          const documentStart = new Position(0, 0)
           const documentEnd = document.lineAt(resourceLength - 1).range.end
-          builder.replace(new vscode.Range(documentStart, documentEnd), text)
+          builder.replace(new Range(documentStart, documentEnd), text)
         })
         .then((success) => {
           if (success) {
@@ -140,12 +202,12 @@ export const removeAllEmptyLine = () =>
               )}ms`
             )
 
-            vscode.window.showInformationMessage(
+            window.showInformationMessage(
               `remove ${resourceLength - document.lineCount} emptyLine ✅`
             )
           }
         })
     } catch (error) {
-      vscode.window.showErrorMessage(`remove emptyLine error ❌`)
+      window.showErrorMessage(`remove emptyLine error ❌`)
     }
   })
